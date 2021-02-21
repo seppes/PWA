96f5fff3d73ff5cb919e127c9f0b737045939c3d
seppe <r0787714@thomasmore.be>
2021-02-21 13:50:12 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+if("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log("SW registered!");
+        console.log(registration);
+
+    }).catch(error => {
+        console.log("SW registration failed!");
+        console.log(error);
+    });
+}else{
+
+}
+
+function chBackcolor(color) {
+    document.body.style.background = color;
+}
